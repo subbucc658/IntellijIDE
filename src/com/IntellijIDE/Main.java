e4ae10a977a86e8537256cd404078a8dacaff43b
Subramanian <subbucc658@gmail.com>
2020-04-26 18:57:29 -0400

    changes to main class

--- src/com/IntellijIDE/Main.java
+++ src/com/IntellijIDE/Main.java
@@ -1,4 +1,5 @@
 package com.IntellijIDE;
+import com.subbu.MathOp.MathOperation;
 import com.subbu.calculator.*;
 
 
@@ -6,7 +7,9 @@ public class Main {
 
     public static void main(String[] args) {
         Calculator calc = new Calculator();
+        MathOperation math = new MathOperation();
         Integer result = calc.Add(10,15);
+        System.out.println(math.Big(20,40));
 
         System.out.println(result);
     }
